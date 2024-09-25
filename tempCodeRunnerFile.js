c06829f41750c6a9f69f24e2778ef0b4a36058f5
Siddhartha34y <143066007+Siddhartha34y@users.noreply.github.com>
2024-09-26 00:44:40 +0530

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,30 @@
+console.log("lets write Javascript")
+
+async function getSongs() {
+
+    let a= await fetch("http://127.0.0.1:5500/songs/")
+    let response= await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs= []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }  
+    }
+    return songs
+}
+
+async function main() {
+    //get the list of the song
+    let songs = await getSongs()
+    console.log(songs)
+
+    //play they first song
+    var audio = new Audio(songs[0]);
+audio.play();
+}
+
+main()
